b6def5a3b2825f4c6492267b3aef87c24f2de0e6
tarunsesetti703 <tarunsesetti703@gmail.com>
2026-05-18 17:08:08 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,18 @@
+'''import mymodule
+mymodule.greetings("tarun")'''
+
+'''import mymodule'''
+
+'''import mymodule'''
+
+'''import mymodule
+a=mymodule.details["idnos"]
+b=mymodule.details["names"]
+print(a)
+print(b)'''
+
+'''for i in mymodule.details:
+    print(i)'''
+
+'''import mymodule'''
+
